1728cbf4b25c94eecc84a5c849a007108156bb98
James Newman <jamesmnewman1@gmail.com>
2016-04-25 15:08:56 -0700

    env var's for production

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'bootstrap-sass', '3.2.0.0'
 gem 'will_paginate',           '3.0.7'
 gem 'bootstrap-will_paginate', '0.0.10'
 gem 'faker'
+gem 'figaro'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,12 @@ Rails.application.configure do
 
 
 
-  # email
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 end
