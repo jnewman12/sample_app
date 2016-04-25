09a34d9799219a16ff4779e288e89fea64257354
James Newman <jamesmnewman1@gmail.com>
2016-04-25 15:50:11 -0700

    host error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
+  host = 'protected-reef-61204.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
