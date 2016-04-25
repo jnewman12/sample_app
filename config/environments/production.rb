7fe3c46c6ce7a93a36c9522b3688914174c8deda
James Newman <jamesmnewman1@gmail.com>
2016-04-25 15:23:54 -0700

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'protected-reef-61204.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
-  #config.action_mailer.default_url_options = { :host => 'example.com' }
-  Rails.application.routes.default_url_options[:host] = 'protected-reef-61204.herokuapp.com'
 end
