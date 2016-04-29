df3b41c51b76146144dae89bd63392cd58c3add4
James Newman <jamesmnewman1@gmail.com>
2016-04-29 16:09:55 -0700

    all tests pass..finished

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -87,4 +87,22 @@ class UserTest < ActiveSupport::TestCase
     james.unfollow(archer)
     assert_not james.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    james = users(:james)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert james.feed.include?(post_following)
+    end
+    # Posts from self
+    james.microposts.each do |post_self|
+      assert james.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not james.feed.include?(post_unfollowed)
+    end
+  end
 end  
